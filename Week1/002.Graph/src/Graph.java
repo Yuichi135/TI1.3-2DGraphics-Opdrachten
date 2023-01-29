b824795122f236a92e006d243e2c8c2557cd3f71
Yuichi Hezemans <61275852+Yuichi135@users.noreply.github.com>
2023-01-29 16:57:50 +0100

    Opdracht 002.Graph

--- Week1/002.Graph/src/Graph.java
+++ Week1/002.Graph/src/Graph.java
@@ -9,9 +9,10 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Graph extends Application {
+    private Canvas canvas;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        this.canvas = new Canvas(640, 400);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Graph");
@@ -20,6 +21,32 @@ public class Graph extends Application {
     
     
     public void draw(FXGraphics2D graphics) {
+        graphics.translate(this.canvas.getWidth()/2, this.canvas.getHeight()/2);
+//        graphics.scale( 1, -1);
+
+        graphics.setColor(Color.RED);
+        graphics.draw(new Line2D.Double(-1000, 0, 1000, 0));
+        graphics.setColor(Color.GREEN);
+        graphics.draw(new Line2D.Double(0, -1000, 0, 1000));
+        graphics.setColor(Color.BLACK);
+
+        double resolution = 0.0001;
+        double scale = 50.0;
+        double lastY = -10 * -10 * -10;
+
+        Long start = System.nanoTime();
+
+        for(double x = -10; x < 10; x += resolution)
+        {
+            // Pow is traag (200001), time in nanos
+            double y = x * x * x; // 38137000, 34192500, 35649600, 36339700, 35290900
+//            double y = Math.pow(x, 3); // 43370400, 41599100, 43204400, 43786000, 42949400
+            graphics.draw(new Line2D.Double(x*scale, y*scale, (x-resolution)*scale, lastY*scale));
+            lastY = y;
+        }
+
+        Long end = System.nanoTime();
+        System.out.println("Total time: " + (end - start));
     }
     
     
