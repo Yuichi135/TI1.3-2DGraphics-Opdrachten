c034e7bf606e386a7825a4645b7a523e6d2c2c3d
Yuichi Hezemans <61275852+Yuichi135@users.noreply.github.com>
2023-01-30 18:58:34 +0100

    Opdracht 001.Moon

--- Week2/001.Moon/src/Moon.java
+++ Week2/001.Moon/src/Moon.java
@@ -8,6 +8,8 @@ import static javafx.application.Application.launch;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.transform.Affine;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
@@ -30,9 +32,18 @@ public class Moon extends Application {
 
     public void draw(FXGraphics2D graphics)
     {
-        graphics.setTransform(new AffineTransform());
+        AffineTransform affineTransform = new AffineTransform();
+        affineTransform.translate(canvas.getWidth() / 2, canvas.getHeight() / 2);
+        graphics.setTransform(affineTransform);
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        Ellipse2D moon = new Ellipse2D.Double(-50, -50, 100.0, 100.0);
+        Ellipse2D moonGap = new Ellipse2D.Double(-80, -60, 100, 100.0);
+        Area area = new Area(moon);
+        area.subtract(new Area(moonGap));
+
+        graphics.fill(area);
     }
 
 
