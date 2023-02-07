9bc4496c63fdab6f3efabd7875d53899e9e94b0e
Yuichi Hezemans <61275852+Yuichi135@users.noreply.github.com>
2023-02-07 12:24:35 +0100

    Opdracht 001.Rainbow

--- Week3/001.Rainbow/src/Rainbow.java
+++ Week3/001.Rainbow/src/Rainbow.java
@@ -16,8 +16,7 @@ public class Rainbow extends Application {
     private ResizableCanvas canvas;
 
     @Override
-    public void start(Stage stage) throws Exception
-    {
+    public void start(Stage stage) throws Exception {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
@@ -28,16 +27,31 @@ public class Rainbow extends Application {
     }
 
 
-    public void draw(FXGraphics2D graphics)
-    {
+    public void draw(FXGraphics2D graphics) {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        graphics.translate(canvas.getWidth() / 2, canvas.getHeight() / 2);
+
+        graphics.setFont(new Font("Arial", Font.PLAIN, 64));
+
+        char[] chars = "Regenboog".toCharArray();
+        graphics.rotate(Math.PI * .5 - Math.PI * (1.0/(chars.length)));
+        graphics.translate(0, 100);
+
+        for (int i = 0; i < chars.length; i++) {
+            graphics.setColor(Color.getHSBColor(i/(chars.length - 1f), 1, 1));
+            graphics.translate(0, -100);
+            graphics.rotate(Math.PI * (1.0/(chars.length)));
+            graphics.translate(0, 100);
+            graphics.rotate(Math.PI);
+            graphics.drawChars(chars, i, 1, 0, 0);
+            graphics.rotate(Math.PI);
+        }
     }
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(Rainbow.class);
     }
 
